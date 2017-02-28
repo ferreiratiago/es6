07a6c8f931d19012c5f984cb727c90887e703ab8
Tiago Ferreira <ferreiratiago@outlook.com>
2017-02-28 13:59:47 +0000

    Added blocks

---
+++ blocks.js
@@ -0,0 +1,30 @@
+/** Blocks **/
+
+// ES5
+// In order to have a block scope, we would use
+// IIFEs (Immediately Invoked Function Expression).
+
+(function IIFE () {
+    // Begin block scope
+    var foo = 'Mr.Foo'
+
+    // End block scope
+})()
+
+foo // Reference Error
+
+// ES6
+// Use {} + let or const for block scope
+
+{
+    // Begin block scope
+    var zed = 'Mr.Zed'
+    let foo = 'Mr.Foo'
+  const bar = 'Mr.Bar'
+
+    // End block scope
+}
+
+zed // Mr.Zed
+foo // Reference Error
+bar // Reference Errror
